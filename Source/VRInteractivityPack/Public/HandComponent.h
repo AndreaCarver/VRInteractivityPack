fb83931dd5b617e5d31ee373ebdaff454bf99bc9
AnsonAdams <ansonadams27@gmail.com>
2018-02-28 10:25:44 -0800

    HandComponent field decs, EHand enum

--- Source/VRInteractivityPack/Public/HandComponent.h
+++ Source/VRInteractivityPack/Public/HandComponent.h
@@ -1,20 +1,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GrabPointEnums.h"
+#include "InteractivityComponent.h"
 #include "Components/ActorComponent.h"
 #include "HandComponent.generated.h"
+
+
+/**
+* Data type for distinguishing between right and left hand.
+*/
+UENUM(BlueprintType)
+enum class EHand : uint8 {
+	H_left	UMETA(DisplayName = "Left"),
+	H_right	UMETA(DisplayName = "Right")
+};
+
 
 /**
 * C++ base class for hand/motion controller component. Can create Blueprint subclasses to add custom functionality.
 */
-UCLASS(ClassGroup=(VRInteractivityPack), meta=(IsBlueprintBase = "true"))
-class VRINTERACTIVITYPACK_API UHandComponent : public UActorComponent
-{
+UCLASS(ClassGroup = (VRInteractivityPack), meta = (IsBlueprintBase="true"))
+class VRINTERACTIVITYPACK_API UHandComponent : public UActorComponent {
+	
 	GENERATED_BODY()
 
 public:	
 	UHandComponent();
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hand")
+	EHand Hand = EHand::H_left;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hand")
+	USkeletalMeshComponent* HandMesh;
+
+	UPROPERTY()
+	UInteractivityComponent* GrabbedInteractivityComponent;
 
 protected:
 	virtual void BeginPlay() override;
